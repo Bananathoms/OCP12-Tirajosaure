f34345af9b7914f200b3e9febd02327ce7bca4a8
Bananathoms <116547351+Bananathoms@users.noreply.github.com>
2024-06-02 17:44:07 +0200

    Initial Commit

---
+++ Tirajosaure/TirajosaureApp.swift
@@ -0,0 +1,17 @@
+//
+//  TirajosaureApp.swift
+//  Tirajosaure
+//
+//  Created by Thomas Carlier on 02/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TirajosaureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
