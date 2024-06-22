9d1dbd0a873f6362fade9cc664258fac42b25e54
Bananathoms <116547351+Bananathoms@users.noreply.github.com>
2024-06-22 21:51:28 +0200

    Update Services

--- Tirajosaure/Common/Enum/CodingKeys.swift
+++ Tirajosaure/Common/Enum/CodingKeys.swift
@@ -2,15 +2,7 @@
 //  CodingKeys.swift
 //  Tirajosaure
 //
-//  Created by Thomas Carlier on 21/06/2024.
+//  Created by Thomas Carlier on 22/06/2024.
 //
 
-enum CodingKeys: String, CodingKey {
-    case objectId
-    case createdAt
-    case updatedAt
-    case ACL
-    case title
-    case options
-    case user
-}
+import Foundation
